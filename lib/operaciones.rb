d8932d6ba773c6a8e86362525e1a738b9801cb4d
Elías Rodríguez Martín <alu0100456608@ull.edu.es>
2013-12-12 11:59:47 +0000

    Añadida clase para definir operaciones DSL

---
+++ lib/operaciones.rb
@@ -0,0 +1,49 @@
+require 'practica9.rb'
+include  Practica9
+
+class Operaciones
+
+   Densa = "Densa"
+   Dispersa = "Dispersa"
+   attr_accessor :cont
+
+   def initialize(name = "", &block)
+      @cont = 0
+      @valor = ""
+      instance_eval &block
+      
+   end
+   def operacion(tipo,lista)
+      vector = Array.new
+      matrices = lista.each {|key, value| vector << matriz(key,value)}
+      tmp = vector.pop()
+      case tipo
+      when "suma"
+         valor = vector.each{|a| tmp = a + tmp}
+      when "resta"
+         valor = vector.each{|a| tmp = a - tmp}
+      when "multipliacion"
+         valor = vector.each{|a| tmp = a * tmp}
+      end
+      @valor  << tipo  << "\n" << tmp.to_s.to_s << "\n"
+   end
+   def densa(f,c)
+      @cont = @cont + 1;
+      [@cont,"Densa",f,c]
+   end
+   def dispersa(f,c)
+      @cont = @cont + 1;
+      [@cont,"Dispersa",f,c]
+   end
+   def matriz(tipo,valor)
+      if tipo[1] == "Densa"
+         return MatrizDensa.new(tipo[2],tipo[3],valor)
+      end
+      if tipo[1] == "Dispersa"
+         return MatrizDispersa.new(tipo[2],tipo[3],valor)
+      end
+   end
+   def to_s
+      @valor
+   end
+end 
